ff98277d37ece32564574e884c1c6a5c40ed6e35
SandraAnghel <sandra.anghel2701@gmail.com>
2021-03-15 19:31:36 +0000

    Working with variables

--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -3,5 +3,6 @@ package com.company;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Variables.displayVariables();
     }
 }
---
+++ src/com/company/Variables.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Variables {
+    public static void displayVariables(){
+        int a1 = 5;
+        float a2 = 5.99f;
+        char a3 = 'a';
+        boolean a4 = true;
+        boolean a5 = false;
+        String a6 = "Hello";
+        byte a7 = Byte.MAX_VALUE;
+        int a8 = Integer.MIN_VALUE;
+        short a9 = 0;
+        int a10 = 9;
+        String a11 = null;
+        int a12;
+        float a13;
+        double a14;
+        char a15;
+        boolean a16;
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+        System.out.println(a5);
+        System.out.println(a6);
+        System.out.println(a7);
+        System.out.println(a8);
+        System.out.println(a9);
+        System.out.println(a10);
+        System.out.println(a11);
+       // System.out.println(a11.length());
+    }
+}
