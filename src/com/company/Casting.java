776947e69e5fa68f378a15b0f63eb71731fbf2df
SandraAnghel <sandra.anghel2701@gmail.com>
2021-03-16 12:06:28 +0000

    Working with implicit and explicit casting

--- src/com/company/Casting.java
+++ src/com/company/Casting.java
@@ -2,10 +2,37 @@ package com.company;
 
 public class Casting {
     public static void doImplicitCasting(){
+        int i1 = 9;
+        double d1 = i1;
+        System.out.println(i1);
+        System.out.println(d1);
 
+        byte b1 = 127;
+        short s1 = b1;
+        System.out.println(b1);
+        System.out.println(s1);
+
+        char c1 = 'a';
+        System.out.println(c1);
+        int i2 = c1;
+        System.out.println(i2);
     }
 
     public static void doExplicitCasting(){
+        double d2 = 12.4567345623;
+        float f1 = (float) d2;
+        System.out.println(d2);
+        System.out.println(f1);
+
+        float f2 = 14.123456f;
+        long l1 = (long) f2;
+        System.out.println(f2);
+        System.out.println(l1);
+
+        int i3 = 90;
+        char c2 = (char) i3;
+        System.out.println(i3);
+        System.out.println(c2);
 
     }
 }
--- src/com/company/Program.java
+++ src/com/company/Program.java
@@ -3,6 +3,11 @@ package com.company;
 public class Program {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("-----Variables-----");
         Variables.displayVariables();
+        System.out.println("-----Implicit Casting-----");
+        Casting.doImplicitCasting();
+        System.out.println("-----Explicit Casting-----");
+        Casting.doExplicitCasting();
     }
 }
